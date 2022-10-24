b261e1289f275beb09c2ad76ba6391d5a498f33b
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2022-10-24 18:13:13 +0900

    전통주 프로젝트 시작

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+app.use(express.static( './src/public' ));
+
+app.get('/index', async(req,res) => {
+    res.render('index.ejs');
+})
+app.listen(3000, async() => console.log(`3000 server is open!`))
---
+++ project.md
@@ -0,0 +1,23 @@
+# project 기능정리
+
+## 페이지
+- USER(사용자)
+    - Main(GET : /index)
+    - Login(GET : /local/index)
+    - Register(GET : /new/index)
+    - myPage(GET : /my-pages/index)
+    - logout(GET : /logout)
+    - 취향(GET : /drink-type/index)
+        - 질문 8개(db에 넣어둠)
+        - 
+    - 주점(GET : /drink-shop/index)
+
+- ADMIN(관리자)
+    - 질문관리
+    - 유져관리
+    - 술관리
+
+
+## 기능
+
+## DATABASE
