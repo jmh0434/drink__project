bb33a0d841bd1b2df868646e24e6abc4e845bae8
jeeeee_sung <85999976+jisung24@users.noreply.github.com>
2022-10-25 14:02:49 +0900

    item property && value 서버에서 받아오기

---
+++ src/models/Drink.js
@@ -0,0 +1,60 @@
+'use strict';
+const mongoose = require('mongoose');
+const drinkSchema = new mongoose.Schema(
+    {
+        title : {
+            type : String,
+            unique : true,
+            required : true,
+        },
+        price : {
+            type : Number,
+            required : true,
+            min : 5000,
+            max : 1000000,
+        },
+        img : {
+            type : String,
+        },
+        type : {
+            // 이걸로 정렬기능 넣을거니까 중요합니다..!
+            type : String,
+            required : true,
+        },
+        alchol : { // 도수
+            type : Number,
+            required : true,
+        },
+        sweet : {
+            type : Number,
+            required : true,
+            min : 1,
+        },
+        sour : {
+            type : Number,
+            required : true,
+            min : 1,
+        },
+        body : {
+            type : Number,
+            required : true,
+            min : 1,
+        },
+        cool : {
+            type : Number,
+            required : true,
+            min : 1,
+        },
+        food : {
+            type : String,
+            // required : false로 해줌. => 입력받을 때 받는게 아니라 food에서 가져오는거라서!
+            required : false,
+        }
+    },
+    {
+        timestamps : true,
+    }
+)
+
+module.exports = mongoose.model('Drink', drinkSchema);
+// 대문자 풀고 s붙여서 collection만듦.
--- src/routes/indexPageRouter.js
+++ src/routes/indexPageRouter.js
@@ -1,13 +1,22 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const Drink = require('../models/Drink.js'); //db에서 불러와주기!! 
 
 module.exports = () => {
 
     // 메인페이지 GET /index
     router.get('/index', async(req,res) => {
         try{
-            let mainPage = await res.status(200).render('index.ejs');
+            let key = await Drink.findOne().select('-_id -id -food -img -body -cool');
+            let drink = await Drink.find().limit(12);
+            let keyArr = Object.keys(key.toJSON());
+            
+            let mainPage = await res.status(200)
+            .render('index.ejs', {
+                drink : drink,
+                keyArr : keyArr,
+            });
         }catch(err){
             return console.log(err);
         }
